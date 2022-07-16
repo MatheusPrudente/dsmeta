bb181cbdeddd2b76764fa0678b3f7415f3016b7c
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2022-07-16 03:40:42 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios from "axios";
 import notification from "../../assets/img/notification-icon.svg";
-import './styles.css';
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios.get(`${BASE_URL}/sales/${id}/notification`).then((response) => {});
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
     <>
-      <div className="dsmeta-red-btn-container">
+      <div
+        className="dsmeta-red-btn-container"
+        onClick={() => handleClick(saleId)}
+      >
         <div className="dsmeta-red-btn">
           <img src={notification} alt="Notificar agente" />
         </div>
